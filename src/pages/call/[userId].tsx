445112c5d6ea4c06ec5c7d50e93bdc5391ebc9b6
Ethen1264 <jxorhks86909763@gmail.com>
2025-08-03 11:38:40 +0900

    feature: call 페이지 번역 적용

--- src/pages/call/[userId].tsx
+++ src/pages/call/[userId].tsx
@@ -1,5 +1,14 @@
 import { VideoCallWidget } from '@/widgets/video-call/VideoCallWidget';
+import { GetServerSideProps } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function CallPage() {
   return <VideoCallWidget />;
 }
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'ko', ['video-call'])),
+    },
+  };
+};
