71ec90af3797780c03dc91b6c21cfeb8a1b2253f
Ethen1264 <jxorhks86909763@gmail.com>
2025-09-04 16:09:01 +0900

    feat: connection server & client socket

--- src/features/chat/model/useChat.ts
+++ src/features/chat/model/useChat.ts
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { socket } from '@/shared/api/socket';
+import { getAccessToken } from '@/shared/api/tokenService';
+import { useEffect, useState } from 'react';
 
 export type Message = {
   id: number | string;
@@ -10,29 +12,26 @@ export type Message = {
 
 export function useChat(userName: string) {
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      id: 1,
-      sender: userName,
-      content: '안녕하세요!',
-      time: '오전 1:42',
-      isMe: false,
-    },
-    {
-      id: 2,
-      sender: '나',
-      content: '안녕하세요! 잘 지내시나요?',
-      time: '오전 1:42',
-      isMe: true,
-    },
-    {
-      id: 3,
-      sender: userName,
-      content: '네, 잘 지내고 있어요. 오늘 날씨가 좋네요!',
-      time: '오전 1:43',
-      isMe: false,
-    },
-  ]);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const token = getAccessToken();
+
+    socket.on('connect', () => {
+      const formattedToken = `Bearer ${token}`;
+      console.log('connected, sending token:', formattedToken);
+      socket.emit('authenticate', { token: formattedToken });
+    });
+
+    socket.on('auth_error', (err) => {
+      console.error('auth_error:', err);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('auth_error');
+    };
+  }, []);
 
   const sendMessage = () => {
     if (message.trim()) {
